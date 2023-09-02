8cfbd0bb5ab1757496c1a612385d5f37e40c801d
emanuelefavero <emafaux89@gmail.com>
2023-09-02 09:14:07 +0200

    add poll variables, jsx and handleAnserClick

--- app/components/poll.tsx
+++ app/components/poll.tsx
@@ -1,3 +1,59 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function Poll() {
-  return <>Poll</>
+  const question = 'What is the capital city of France?'
+  const answers = [
+    {
+      text: 'London',
+      votes: 100,
+    },
+    {
+      text: 'Paris',
+      votes: 200,
+    },
+    {
+      text: 'Berlin',
+      votes: 66,
+    },
+  ]
+  const totalVotes = 366
+  const correctAnswer = 1
+  const correctAnswerExplanation = 'Paris is the capital city of France.'
+
+  const [selectedAnswer, setSelectedAnswer] = useState(-1)
+  const [showCorrectAnswer, setShowCorrectAnswer] = useState(false)
+
+  const handleAnswerClick = (index: number) => {
+    // If the answer clicked is equal to the already selected answer, deselect it and hide the correct answer explanation
+    if (index === selectedAnswer) {
+      setSelectedAnswer(-1)
+      setShowCorrectAnswer(false)
+    } else {
+      setSelectedAnswer(index)
+      setShowCorrectAnswer(true)
+    }
+  }
+
+  return (
+    <section>
+      <h1>{question}</h1>
+      <ul>
+        {answers.map((answer, index) => {
+          const percentage = Math.round((answer.votes / totalVotes) * 100)
+          return (
+            <li key={index}>
+              <button onClick={() => handleAnswerClick(index)}>
+                <p>{answer.text}</p>
+                <span>{percentage}%</span>
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+      <p>{totalVotes}</p> answered
+      {showCorrectAnswer && <p>{correctAnswerExplanation}</p>}
+    </section>
+  )
 }
